6602938e7cb683143a3bcfc5192e00ecf238dba4
howCanIFind <second9000@naver.com>
2022-10-31 09:20:13 +0900

    프로젝트 생성

---
+++ src/main/java/com/project/booktalking/BookTalkingApplication.java
@@ -0,0 +1,13 @@
+package com.project.booktalking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookTalkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookTalkingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/booktalking/BookTalkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.booktalking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookTalkingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
