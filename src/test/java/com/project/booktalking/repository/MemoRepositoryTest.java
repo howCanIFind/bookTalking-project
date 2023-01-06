ffbdc33aa3a5644d0fb1cf992ffcf3b74fd1aa27
howCanIFind <second9000@naver.com>
2023-01-06 18:10:32 +0900

    bookRepo주석처리

--- src/test/java/com/project/booktalking/repository/MemoRepositoryTest.java
+++ src/test/java/com/project/booktalking/repository/MemoRepositoryTest.java
@@ -16,142 +16,142 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @DataJpaTest
 class MemoRepositoryTest {
-//
-//    @Autowired
-//    private MemoRepository memoRepository;
-//    @Autowired
-//    private BookRepository bookRepository;
-//
-//    // 데이터 준비
-//    @BeforeEach
-//    public void 데이터준비() {
-//        String name = "bookName";
-//        String author = "author";
-//        String summary = "summary";
-//        String companyName = "companyName";
-//        Book book = Book.builder()
-//                .name(name)
-//                .author(author)
-//                .summary(summary)
-//                .companyName(companyName)
-//                .build();
-//        bookRepository.save(book);
-//
-//        String content = "content";
-//        Memo memo = Memo.builder()
-//                .content(content)
-//                .book(book)
-//                .build();
-//        memoRepository.save(memo);
-//
-//    }
-//
-//    // 저장
-//    @Test
-//    public void 메모등록_테스트() {
-//        // given
-//        String name = "bookName2";
-//        String author = "author2";
-//        String summary = "summary2";
-//        String companyName = "companyName2";
-//        Book book = Book.builder()
-//                .name(name)
-//                .author(author)
-//                .summary(summary)
-//                .companyName(companyName)
-//                .build();
-//        bookRepository.save(book);
-//
-//        String content = "content2";
-//        Memo memo = Memo.builder()
-//                .content(content)
-//                .book(book)
-//                .build();
-//
-//        // when
-//        Memo memoPs = memoRepository.save(memo);
-//
-//        // then
-//        assertThat(content).isEqualTo(memoPs.getContent());
-//        assertThat(book).isEqualTo(memoPs.getBook());
-//    }
-//
-//    // 조회
-//    @Test
-//    public void 메모조회_테스트() {
-//        // given
-//        Long memoId = 2L;
-//
-//        String content = "content";
-//
-//        String name = "bookName";
-//        String author = "author";
-//        String summary = "summary";
-//        String companyName = "companyName";
-//
-//        // when
-//        Memo memoPs = memoRepository.findById(memoId).get();
-//
-//        // then
-//        assertThat(content).isEqualTo(memoPs.getContent());
-//        assertThat(name).isEqualTo(memoPs.getBook().getName());
-//        assertThat(author).isEqualTo(memoPs.getBook().getAuthor());
-//        assertThat(summary).isEqualTo(memoPs.getBook().getSummary());
-//        assertThat(companyName).isEqualTo(memoPs.getBook().getCompanyName());
-//    }
-//
-//
-//    @Test
-//    public void 메모목록조회byBook_테스트() {
-//        // given
-//        String content = "content";
-//
-//        String name = "bookName";
-//        String author = "author";
-//        String summary = "summary";
-//        String companyName = "companyName";
-//
-//        Book book = bookRepository.findAll().get(0);
-//
-//        // when
-//        List<Memo> memoList = memoRepository.findAllByBook(book);
-//
-//        // then
-//        assertThat(content).isEqualTo(memoList.get(0).getContent());
-//        assertThat(name).isEqualTo(memoList.get(0).getBook().getName());
-//        assertThat(author).isEqualTo(memoList.get(0).getBook().getAuthor());
-//        assertThat(summary).isEqualTo(memoList.get(0).getBook().getSummary());
-//        assertThat(companyName).isEqualTo(memoList.get(0).getBook().getCompanyName());
-//    }
-//
-//    // 삭제
-//    @Test
-//    public void 메모삭제_테스트() {
-//        // given
-//        Long id = memoRepository.findAll().get(0).getId();
-//
-//        // when
-//        memoRepository.deleteById(id);
-//
-//        // then
-//        assertFalse(bookRepository.findById(id).isPresent());
-//    }
-//
-//    // 수정
-//    @Test
-//    public void 메모수정_테스트() {
-//        // given
-//        Long memoId = memoRepository.findAll().get(0).getId();
-//
-//        String content = "content 수정";
-//        MemoReqDto memoReqDto = new MemoReqDto(content);
-//
-//        // when
-//        Memo memo = memoRepository.findById(memoId).get();
-//        memo.update(memoReqDto);
-//
-//        // then
-//        assertThat(content).isEqualTo(memo.getContent());
-//
-//    }
+
+    @Autowired
+    private MemoRepository memoRepository;
+    @Autowired
+    private BookRepository bookRepository;
+
+    // 데이터 준비
+    @BeforeEach
+    public void 데이터준비() {
+        String name = "bookName";
+        String author = "author";
+        String summary = "summary";
+        String companyName = "companyName";
+        Book book = Book.builder()
+                .name(name)
+                .author(author)
+                .summary(summary)
+                .companyName(companyName)
+                .build();
+        bookRepository.save(book);
+
+        String content = "content";
+        Memo memo = Memo.builder()
+                .content(content)
+                .book(book)
+                .build();
+        memoRepository.save(memo);
+
+    }
+
+    // 저장
+    @Test
+    public void 메모등록_테스트() {
+        // given
+        String name = "bookName2";
+        String author = "author2";
+        String summary = "summary2";
+        String companyName = "companyName2";
+        Book book = Book.builder()
+                .name(name)
+                .author(author)
+                .summary(summary)
+                .companyName(companyName)
+                .build();
+        bookRepository.save(book);
+
+        String content = "content2";
+        Memo memo = Memo.builder()
+                .content(content)
+                .book(book)
+                .build();
+
+        // when
+        Memo memoPs = memoRepository.save(memo);
+
+        // then
+        assertThat(content).isEqualTo(memoPs.getContent());
+        assertThat(book).isEqualTo(memoPs.getBook());
+    }
+
+    // 조회
+    @Test
+    public void 메모조회_테스트() {
+        // given
+        Long memoId = 2L;
+
+        String content = "content";
+
+        String name = "bookName";
+        String author = "author";
+        String summary = "summary";
+        String companyName = "companyName";
+
+        // when
+        Memo memoPs = memoRepository.findById(memoId).get();
+
+        // then
+        assertThat(content).isEqualTo(memoPs.getContent());
+        assertThat(name).isEqualTo(memoPs.getBook().getName());
+        assertThat(author).isEqualTo(memoPs.getBook().getAuthor());
+        assertThat(summary).isEqualTo(memoPs.getBook().getSummary());
+        assertThat(companyName).isEqualTo(memoPs.getBook().getCompanyName());
+    }
+
+
+    @Test
+    public void 메모목록조회byBook_테스트() {
+        // given
+        String content = "content";
+
+        String name = "bookName";
+        String author = "author";
+        String summary = "summary";
+        String companyName = "companyName";
+
+        Book book = bookRepository.findAll().get(0);
+
+        // when
+        List<Memo> memoList = memoRepository.findAllByBook(book);
+
+        // then
+        assertThat(content).isEqualTo(memoList.get(0).getContent());
+        assertThat(name).isEqualTo(memoList.get(0).getBook().getName());
+        assertThat(author).isEqualTo(memoList.get(0).getBook().getAuthor());
+        assertThat(summary).isEqualTo(memoList.get(0).getBook().getSummary());
+        assertThat(companyName).isEqualTo(memoList.get(0).getBook().getCompanyName());
+    }
+
+    // 삭제
+    @Test
+    public void 메모삭제_테스트() {
+        // given
+        Long id = memoRepository.findAll().get(0).getId();
+
+        // when
+        memoRepository.deleteById(id);
+
+        // then
+        assertFalse(bookRepository.findById(id).isPresent());
+    }
+
+    // 수정
+    @Test
+    public void 메모수정_테스트() {
+        // given
+        Long memoId = memoRepository.findAll().get(0).getId();
+
+        String content = "content 수정";
+        MemoReqDto memoReqDto = new MemoReqDto(content);
+
+        // when
+        Memo memo = memoRepository.findById(memoId).get();
+        memo.update(memoReqDto);
+
+        // then
+        assertThat(content).isEqualTo(memo.getContent());
+
+    }
 }
